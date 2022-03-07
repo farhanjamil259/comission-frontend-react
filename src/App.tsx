ce52f06a2db9a91295b2218d5822ae7d1c11ba8c
System Administrator <root@Farhans-MacBook-Pro.local>
2022-03-08 04:43:07 +0500

    feat: added design pages and improved general navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { StoreProvider } from "./redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Login from "./pages/auth/Login";
 import Home from "./pages/Home";
+import { dashboardRoutes } from "./AppRoutes";
 
 const App = (): React.ReactElement => {
   return (
@@ -11,6 +11,11 @@ const App = (): React.ReactElement => {
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
+        {dashboardRoutes.map((route, routeIndex) => {
+          return (
+            <Route key={routeIndex} path={route.path} element={route.page} />
+          );
+        })}
       </Route>
     </Routes>
   );
