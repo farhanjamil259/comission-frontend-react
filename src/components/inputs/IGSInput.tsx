cd94b6c5335b8f93ab389de1597592c8a3cdd4cc
igs4ahsan <ahsan@intelligentsoftware.net>
2022-03-11 18:09:33 +0500

    feat: icons ready

---
+++ src/components/inputs/IGSInput.tsx
@@ -0,0 +1,60 @@
+// import React from "react";
+
+// type InputFieldProps = {
+//   label?: string;
+//   placeholder?: string;
+//   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+// };
+
+// const InputField = ({
+//   label,
+//   placeholder,
+//   onChange,
+// }: InputFieldProps): React.ReactElement => {
+//   return (
+//     <div className="igs-input">
+//       <label className="igs-input--label">{label}</label>
+//       <input
+//         onChange={onChange}
+//         className="igs-input--field"
+//         placeholder={placeholder}
+//       />
+//     </div>
+//   );
+// };
+
+// export default InputField;
+
+import React from "react";
+
+interface IData {
+  key: string;
+  val: string;
+}
+
+interface IIGSInputProps extends React.HTMLProps<HTMLInputElement> {
+  options?: IData[];
+}
+
+const IGSInput = ({
+  type,
+  label,
+  placeholder,
+}: IIGSInputProps): React.ReactElement => {
+  return (
+    <div className="igs-input">
+      <div className="igs-icon--pre"></div>
+      <div className="igs-input--container">
+        <label className="igs-input--label">{label}</label>
+        <input
+          type={type ?? "text"}
+          className="igs-input--field"
+          placeholder={placeholder}
+        />
+      </div>
+      <div className="igs-icon--action"></div>
+    </div>
+  );
+};
+
+export default IGSInput;
