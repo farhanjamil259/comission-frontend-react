97afbf434a39f8690123d0f546750fe98986e2f4
farhanjamil295 <farhanjamil295@gmail.com>
2022-03-12 02:57:59 +0500

    chore: removed commented input code

--- src/components/inputs/IGSInput.tsx
+++ src/components/inputs/IGSInput.tsx
@@ -1,30 +1,3 @@
-// import React from "react";
-
-// type InputFieldProps = {
-//   label?: string;
-//   placeholder?: string;
-//   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-// };
-
-// const InputField = ({
-//   label,
-//   placeholder,
-//   onChange,
-// }: InputFieldProps): React.ReactElement => {
-//   return (
-//     <div className="igs-input">
-//       <label className="igs-input--label">{label}</label>
-//       <input
-//         onChange={onChange}
-//         className="igs-input--field"
-//         placeholder={placeholder}
-//       />
-//     </div>
-//   );
-// };
-
-// export default InputField;
-
 import React from "react";
 import { IconTypes } from "../icon/Icons";
 import IGSIcon from "../icon/IGSIcon";
