3ed11b2e8f34d27d2c93ff34ad0c34e92772bf37
igs4ahsan <ahsan@intelligentsoftware.net>
2022-03-12 01:21:52 +0500

    feat: input field under progress

--- src/pages/design/AhsanComponents.tsx
+++ src/pages/design/AhsanComponents.tsx
@@ -4,8 +4,11 @@ import IGSInput from "../../components/inputs/IGSInput";
 const AhsanComponents = (): React.ReactElement => {
   return (
     <div>
-      <IGSInput label="Business Name" placeholder="ex: Company ABC" />
+      <input />
+      <IGSInput label="Amount" placeholder="ex: 10,000" />
       <IGSInput
+        preIcon="email"
+        postIcon="lock"
         type="email"
         label="Email Address"
         placeholder="someone@example.com"
