caf93d2e017b8cc1eaaa1a358453fff37c60455e
igs4ahsan <ahsan@intelligentsoftware.net>
2022-03-16 23:19:28 +0500

    refactor: login page updated

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,51 @@
 import React from "react";
+import ActionLink from "../../components/button/ActionLink";
+import Button from "../../components/button/Button";
+import CustomForm from "../../components/form/CustomForm";
+import FormFlex from "../../components/form/FormFlex";
+import CheckboxInput from "../../components/inputs/CheckboxInput";
+import IGSInput from "../../components/inputs/IGSInput";
+import IGSText from "../../components/text/IGSText";
 
 const Login = (): React.ReactElement => {
   return (
     <div className="login-page">
       <div className="login-page-section-left">IMAGE</div>
-      <div className="login-page-section-right">FORM</div>
+      <div className="login-page-section-right">
+        <CustomForm
+          textLeft
+          id="login-form"
+          title="Welcome Back"
+          subtitle="We are excited to see you again!"
+        >
+          <FormFlex column>
+            <IGSInput
+              type="email"
+              label="Email Address"
+              showIcon
+              placeholder="someone@example.com"
+            />
+            <IGSInput
+              type="password"
+              label="Password"
+              showIcon
+              placeholder="secret"
+            />
+            <FormFlex>
+              <CheckboxInput label="Remeber Me" value={true} />
+              <ActionLink text="Forgot Password?" />
+            </FormFlex>
+            <br />
+            <FormFlex>
+              <Button text="Login" variant="primary" rounded />
+              <div>
+                <IGSText type="label-medium">Do not have an account.</IGSText>
+                <ActionLink text="Create Account" />
+              </div>
+            </FormFlex>
+          </FormFlex>
+        </CustomForm>
+      </div>
     </div>
   );
 };
