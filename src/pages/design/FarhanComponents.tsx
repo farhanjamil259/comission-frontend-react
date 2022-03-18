eb7b5480c140870e670c061e46f1cd7133fe1915
farhanjamil295 <farhanjamil295@gmail.com>
2022-03-18 18:36:19 +0500

    fix: lint ts error

--- src/pages/design/FarhanComponents.tsx
+++ src/pages/design/FarhanComponents.tsx
@@ -3,26 +3,7 @@ import SelectInput from "../../components/inputs/SelectInput";
 import RangeInput from "../../components/inputs/RangeInput";
 
 const FarhanComponents = (): React.ReactElement => {
-  return (
-    <div>
-      <SelectInput
-        label="Options"
-        options={[
-          {
-            label: "Test 1",
-            value: "asd1",
-          },
-        ]}
-      <RangeInput
-        type="range"
-        min={0}
-        max={100}
-        rangeValues={[2, 80]}
-        step={10}
-        marks={{ 0: "25M", 100: "100M" }}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default FarhanComponents;
