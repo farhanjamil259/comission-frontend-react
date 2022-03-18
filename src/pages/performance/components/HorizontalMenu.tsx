610ab51e21bd3ca28fa85f3ad709c22313026b02
farhanjamil295 <farhanjamil295@gmail.com>
2022-03-18 21:26:19 +0500

    feat: added horizontal menu active class

--- src/pages/performance/components/HorizontalMenu.tsx
+++ src/pages/performance/components/HorizontalMenu.tsx
@@ -1,18 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const HorizontalMenu = (): React.ReactElement => {
+  const location = useLocation();
+
   return (
     <div className="horizontal-menu">
-      <Link className="horizontal-menu--link" to="/performance/salesoverview">
+      <Link
+        className={`horizontal-menu--link ${
+          location.pathname === "/performance/salesoverview"
+            ? "horizontal-menu--link--active"
+            : ""
+        }`}
+        to="/performance/salesoverview"
+      >
         Sales Overview
       </Link>
 
-      <Link className="horizontal-menu--link" to="/performance/generalinfo">
+      <Link
+        className={`horizontal-menu--link ${
+          location.pathname === "/performance/generalinfo"
+            ? "horizontal-menu--link--active"
+            : ""
+        }`}
+        to="/performance/generalinfo"
+      >
         General Info
       </Link>
 
-      <Link className="horizontal-menu--link" to="/performance/employeedetails">
+      <Link
+        className={`horizontal-menu--link ${
+          location.pathname === "/performance/employeedetails"
+            ? "horizontal-menu--link--active"
+            : ""
+        }`}
+        to="/performance/employeedetails"
+      >
         Contracts
       </Link>
     </div>
