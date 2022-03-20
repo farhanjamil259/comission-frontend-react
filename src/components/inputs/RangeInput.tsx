bebb433e1016d42d8a0272809e3bd34e8c5d4bb9
igs4ahsan <ahsan@intelligentsoftware.net>
2022-03-20 17:12:28 +0500

    refactor: range input improved

--- src/components/inputs/RangeInput.tsx
+++ src/components/inputs/RangeInput.tsx
@@ -6,6 +6,8 @@ const Range = createSliderWithTooltip(RCRange);
 const NewSlider = createSliderWithTooltip(Slider);
 
 import "rc-slider/assets/index.css";
+import IGSText from "../text/IGSText";
+import classNames from "classnames";
 
 type RangeInputProps = {
   rangeValues?: number[];
@@ -21,6 +23,8 @@ type RangeInputProps = {
   marks?: {};
   tipPrefix?: string;
   tipPostfix?: string;
+  label?: string;
+  description?: string;
 };
 
 const generateGradient = (color: RangeInputProps["color"]): string => {
@@ -45,19 +49,46 @@ const RangeInput = (props: RangeInputProps): React.ReactElement => {
     display: props.noHandle ? "none" : "block",
   };
 
+  const coreClass = "igs-range";
+  const textClass = classNames(`${coreClass}__text`, {
+    [`${coreClass}__text--small`]: props.noHandle,
+  });
+
+  const TextSection = (): React.ReactElement => {
+    return (
+      <div>
+        {(props.label || props.description) && (
+          <div className={textClass}>
+            <IGSText type="label-large" className="igs-range__label" tag="p">
+              {props.label}
+            </IGSText>
+            <IGSText type="label-medium" className="igs-range__description">
+              {props.description}
+            </IGSText>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const tipFormatter = (value: number): React.ReactNode => {
+    return (
+      !props.noHandle && (
+        <div>
+          {props.tipPrefix}
+          {value}
+          {props.tipPostfix}
+        </div>
+      )
+    );
+  };
+
   if (props.type === "range") {
     return (
       <div className="igs-range">
+        <TextSection />
         <Range
-          tipFormatter={(value): React.ReactNode => {
-            return (
-              <div>
-                {props.tipPrefix}
-                {value}
-                {props.tipPostfix}
-              </div>
-            );
-          }}
+          tipFormatter={tipFormatter}
           tipProps={{
             visible: true,
             placement: "top",
@@ -100,16 +131,9 @@ const RangeInput = (props: RangeInputProps): React.ReactElement => {
   } else {
     return (
       <div className="igs-range">
+        <TextSection />
         <NewSlider
-          tipFormatter={(value): React.ReactNode => {
-            return (
-              <div>
-                {props.tipPrefix}
-                {value}
-                {props.tipPostfix}
-              </div>
-            );
-          }}
+          tipFormatter={tipFormatter}
           tipProps={{
             visible: true,
             placement: "top",
