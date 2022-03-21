8dd8593a0973505a2fb10c8ccba06d5863c85205
farhanjamil295 <farhanjamil295@gmail.com>
2022-03-21 18:37:03 +0500

    feat: sidenav completed

--- src/pages/performance/components/PerformanceSidenav.tsx
+++ src/pages/performance/components/PerformanceSidenav.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import HorizontalMenu from "./HorizontalMenu";
+import NavProfile from "./NavProfile";
+import NavStats from "./NavStats";
 
 const PerformanceSidenav = (): React.ReactElement => {
   return (
     <div className="performance-sidenav">
+      <NavProfile />
+      <NavStats />
       <HorizontalMenu />
     </div>
   );
