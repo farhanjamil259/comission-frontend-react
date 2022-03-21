6b4bfbb8aa95f966a41cbf129a776199aa28bac5
igs4ahsan <ahsan@intelligentsoftware.net>
2022-03-21 19:05:55 +0500

    refactor: forms updated

--- src/pages/Contracts/components/contractAdd/ContractAddStepFour.tsx
+++ src/pages/Contracts/components/contractAdd/ContractAddStepFour.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import CustomForm from "../../../../components/form/CustomForm";
 import XFlex from "../../../../components/form/FormFlex";
 import Checkbox from "../../../../components/inputs/CheckboxInput";
 import IGSInput from "../../../../components/inputs/IGSInput";
+import RangeInput from "../../../../components/inputs/RangeInput";
 import { TypoText } from "../../../../components/typography/XText";
 
 type CustomFormProps = {
@@ -10,26 +11,50 @@ type CustomFormProps = {
 };
 
 const ContractAddStepFour = (props: CustomFormProps): React.ReactElement => {
+  const [range, setRange] = useState([5, 15]);
+  const [range2, setRange2] = useState([1, 25]);
   return (
     <CustomForm id={props.id} title="Define ACCELERATORS">
-      <XFlex column>
-        <XFlex>
-          <div>
-            <TypoText color="dark-100" marginBottom="small">
-              Enable Retroactive Accelerators?
-            </TypoText>
-            <TypoText size="small" color="dark-200" marginBottom="medium">
-              Apply upper tier commission on all sales.
-            </TypoText>
-          </div>
-          <Checkbox />
-        </XFlex>
-        <IGSInput label="ACCELERATOR" />
-        <XFlex>
-          <IGSInput type="currency" label="Target" />
-          <IGSInput type="percent" label="Extra Commission" />
-        </XFlex>
-      </XFlex>
+      <Checkbox
+        formColumn
+        label=" Enable Retroactive Accelerators?"
+        description="Apply upper accelerator on all sales."
+      />
+      <IGSInput label="ACCELERATOR" />
+      <IGSInput type="percent" label="Extra Commission" />
+      <RangeInput
+        label="SALE TARGET"
+        step={5}
+        min={0}
+        max={100}
+        type="range"
+        rangeValues={range2}
+        onChange={(event): void => {
+          setRange2(event as number[]);
+        }}
+        color="success"
+        marks={{ 0: "0M", 25: "25M", 50: "50M", 75: "75M", 100: "100M" }}
+      />
+      <RangeInput
+        label="MONTH DAYS"
+        min={1}
+        max={30}
+        type="range"
+        rangeValues={range}
+        onChange={(event): void => {
+          setRange(event as number[]);
+        }}
+        color="success"
+        marks={{
+          1: "1st",
+          5: "5th",
+          10: "10th",
+          15: "15th",
+          20: "20th",
+          25: "25th",
+          30: "30th",
+        }}
+      />
     </CustomForm>
   );
 };
--- src/pages/EmployeeDashboard/components/employeeAdd/EmployeeAddStepThree.tsx
+++ src/pages/EmployeeDashboard/components/employeeAdd/EmployeeAddStepThree.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CustomForm from "../../../../components/form/CustomForm";
 import XFlex from "../../../../components/form/FormFlex";
+import Checkbox from "../../../../components/inputs/CheckboxInput";
 import IGSInput from "../../../../components/inputs/IGSInput";
 import RadioInput from "../../../../components/inputs/RadioInput";
 
@@ -11,22 +12,25 @@ type CustomFormProps = {
 const EmployeeAddStepThree = (props: CustomFormProps): React.ReactElement => {
   const radioOptions = [
     {
-      label: "Sales",
-      value: "Direct compensation",
-    },
-    {
       label: "Draw",
       value: "Loan to be repaid",
+    },
+    {
+      label: "Sales",
+      value: "Direct compensation",
     },
   ];
   return (
     <CustomForm id={props.id} title="Payroll Account">
-      <XFlex column>
-        <RadioInput options={radioOptions} />
-        <IGSInput type="currency" label="Amount" />
-        <IGSInput type="calendar-day" label="Pay Day" />
-        <IGSInput type="currency" label="Reserve Limit" />
-      </XFlex>
+      <RadioInput options={radioOptions} />
+      <Checkbox
+        label="Recoverable Draw?"
+        description="When the commission earned is more than the draw, the salesperson return the draw amount."
+        formColumn
+      />
+      <IGSInput type="currency" label="Amount" />
+      <IGSInput type="calendar-day" label="Pay Day" />
+      <IGSInput type="currency" label="Reserve Limit" />
     </CustomForm>
   );
 };
